1fb5d9a7f7b90ef25c52d0dfecb9f72cc903f177
Vini <vinimartinsc@gmail.com>
2019-11-28 06:39:50 -0300

    preparos para database

--- app/src/main/java/br/com/si/ufrrj/MenuPrincipal.kt
+++ app/src/main/java/br/com/si/ufrrj/MenuPrincipal.kt
@@ -30,4 +30,19 @@ class MenuPrincipal : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    //Design - Immersive mode
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) escondeUI() // removendo o system UI
+    }
+
+
+    private fun escondeUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
 }
